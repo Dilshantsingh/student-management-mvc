e02e2eebd6e26e4fb7a3a7b312a0f0e4efec6953
Dilshantsingh <Dilshant2004@gmail.com>
2025-10-05 20:36:43 +0530

    Create Student.js

---
+++ src/models/Student.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const studentSchema = new mongoose.Schema({
+  name: { type: String, required: [true, 'Name is required'], trim: true },
+  age: { type: Number, required: [true, 'Age is required'], min: [0, 'Age must be >= 0'] },
+  course: { type: String, required: [true, 'Course is required'], trim: true }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Student', studentSchema);
